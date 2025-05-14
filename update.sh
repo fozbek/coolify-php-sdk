7f9dabf228c12cdfc5015743344691b622544f80
Fatih Ozbek <mail@fatih.dev>
2025-05-14 17:49:27 +0300

    rename the namespace from `OpenAPI\\Client` to `CoolifyPHP\\Client`

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
   -i https://raw.githubusercontent.com/coollabsio/coolify/refs/heads/v4.x/openapi.yaml \
-
   -g php -o /local \
-  --additional-properties=composerPackageName=fozbek/CoolifyPHP,invokerPackage=CoolifyPHP\\Client
+  --additional-properties=composerPackageName=fozbek/coolify-php-sdk,invokerPackage=CoolifyPHP\\Client
